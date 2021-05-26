5eea33e7584a9942118241f038fdc71d24bd7fc6
ls <49014085+raeunlee@users.noreply.github.com>
2021-05-26 16:57:11 +0900

    commit on 'Master'

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+ 
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2200,
+});
+$('.slideshow').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+});	
+});
+
